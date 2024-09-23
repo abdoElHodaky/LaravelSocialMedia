d2d1809b76ca876f46f71aec383c0a402ee2a6a1
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-23 14:09:10 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
          <meta name="theme-color" content="#6777ef"/>
-         <link rel="favicon" href="{{assets('logo.png')}}">
-         
+        <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
+        <link rel="manifest" href="{{ asset('/manifest.json') }}">
+        
         <title>{{ config('app.name', 'Laravel') }}</title>
         <!-- PWA  -->
             <!-- Fonts -->
@@ -32,6 +33,7 @@
                 @yield('container')
             </main>
         </div>
+        <script src="{{ asset('/sw.js') }}"></script>
 <script>
    if ("serviceWorker" in navigator) {
       // Register a service worker hosted at the root of the
