6dca18e750e21790911ad106195376401f9d377a
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-23 14:39:20 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,55 @@
      console.error("Service workers are not supported.");
   }
 </script>
+    <script>
+    window.addEventListener("beforeinstallprompt", (e) => {
+        // Prevent the mini-infobar from appearing on mobile
+        e.preventDefault();
+
+        // Stash the event so it can be triggered later.
+        window.deferredPrompt = e;
+        console.log("Registerd event");
+        // Update UI notify the user they can install the PWA
+        window.localStorage.setItem("pwainstalled", "false");
+        //this.showInstallPromotion();
+    });
+    window.addEventListener("appinstalled", (evt) => {
+        // Log install to analytics
+        console.log("INSTALL: Success");
+        window.localStorage.setItem("pwainstalled", "true");
+    });
+</script>
+        <script>
+    function installButtonDisplay() {
+        var btn = document.createElement("BUTTON");
+        btn.setAttribute("id", "install-button");
+        btn.innerHTML = "Download Web-App";
+        btn.setAttribute("class","bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center")
+        btn.onclick = function() {
+          installPWA();
+        }
+        document.body.appendChild(btn);
+    }
+    function installPWA() {
+        if (window.deferredPrompt) {
+            console.log("inside window.deferredPromp if condition");
+            window.deferredPrompt.prompt();
+            window.deferredPrompt.userChoice.then((choiceResult) => {
+               if (choiceResult.outcome === "accepted") { 
+                  removeButton();                   
+                  console.log("User accepted the install prompt");
+               } else {
+                  isInstalledState(false);
+                  console.log("User dismissed the install prompt");
+               }
+           });
+        }
+    }
+    function removeButton() {
+        var elem = document.getElementById('install-button');
+        elem.parentNode.removeChild(elem);
+    }
+</script>
    <script src="//cdn.jsdelivr.net/npm/eruda"></script>
  
     <script>eruda.init();</script>
