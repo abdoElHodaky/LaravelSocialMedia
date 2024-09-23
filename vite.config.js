3c27b657ca41276e0a157f40f8ff99f0d96aaca4
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-23 13:36:08 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,12 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'public/manifest.json'
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/js/dashboard.js'
+                'public/sw.js',
+                
             ],
             refresh: true,
         }),
