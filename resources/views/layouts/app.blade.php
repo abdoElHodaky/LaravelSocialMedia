97b8140fe95ef81b62d4b884a6328d6b56a6c02c
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-26 16:15:05 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,23 +33,7 @@
                 @yield('container')
             </main>
         </div>
-        <!--script src="{{ asset('/sw.js') }}"></script>
-<script>
-   if ("serviceWorker" in navigator) {
-      // Register a service worker hosted at the root of the
-      // site using the default scope.
-      navigator.serviceWorker.register("/sw.js").then(
-      (registration) => {
-         console.log("Service worker registration succeeded:", registration);
-      },
-      (error) => {
-         console.error(`Service worker registration failed: ${error}`);
-      },
-    );
-  } else {
-     console.error("Service workers are not supported.");
-  }
-</script-->
+        
    <script src="https://cdn.tailwindcss.com"></script>
       
         <script src="//cdn.jsdelivr.net/npm/eruda"></script>
